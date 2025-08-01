17c3090641434308f8c92f27af7dd882a40a678f
Fadli Ramadhan <fadli16ramadhan@gmail.com>
2025-08-01 13:07:32 +0700

    final

--- src/hooks/useSocket.ts
+++ src/hooks/useSocket.ts
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:3000", {
+export const socket = io("http://https://circle-api-production.up.railway.app", {
     transports: ["websocket"],
     withCredentials: true,
 });
